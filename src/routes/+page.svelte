ef80867c671703034ba9b287b8dcd3a8d77486c5
Hendrik Mans <hendrik@mans.de>
2025-02-10 09:54:03 +0100

    Add LayerChart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import '../app.css';
+	import { AreaChart } from 'layerchart';
+	import { data } from './data';
 </script>
 
 <main class="p-10">
 	<p class="text-2xl">Hello world</p>
+
+	<div class="h-[300px] p-4 border rounded">
+		<AreaChart {data} x="date" y="value" />
+	</div>
 </main>
